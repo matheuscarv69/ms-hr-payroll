04a4afd4679cb1f86baedf82f0c6381183cf4853
matheuscarv69 <matheus9126@gmail.com>
2021-11-02 16:59:42 -0400

    :sparkless: feat: create get worker by id and get all workers

---
+++ src/main/kotlin/src/entities/worker/service/GetWorkerService.kt
@@ -0,0 +1,41 @@
+package src.entities.worker.service
+
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.stereotype.Service
+import src.configs.exception.WorkerNotFoundException
+import src.entities.worker.model.Worker
+import src.entities.worker.repository.WorkerRepository
+import src.entities.worker.usecase.GetWorkerUseCase
+
+@Service
+class GetWorkerService(
+
+    @Autowired
+    private val repository: WorkerRepository
+
+) : GetWorkerUseCase {
+
+    private val log = LoggerFactory.getLogger(this.javaClass)
+
+    override fun getWorkerById(workerId: Long): Worker {
+        log.info("Getting Worker by ID: $workerId")
+
+        return repository.findById(workerId).orElseThrow {
+            WorkerNotFoundException("This Worker ID: $workerId not found.")
+        }
+    }
+
+    override fun getAllWorkers(
+        active: Boolean,
+        pageable: Pageable
+    ): Page<Worker> {
+        log.info("Getting all Workers")
+
+        return repository.findAllByActive(active, pageable)
+    }
+
+
+}
